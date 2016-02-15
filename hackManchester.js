96a283f0ddc5abc5e282d41b29142efa14e60207
ZakaZeko <zakrea.123456789@yahoo.co.uk>
2016-02-15 15:54:44 +0000

    Ininatal commit

---
+++ hackManchester.js
@@ -0,0 +1,65 @@
+Cards = new Mongo.Collection("cards");
+
+
+if (Meteor.isClient) {
+  var myCurrentCard = {};
+
+  Template.deck.helpers({
+    Cards: function(){
+      return Cards.find();
+    }
+  });
+
+  Template.ui.events({
+    'click #btnNewCard': (event, template) => {
+      myCurrentCard = Cards.findOne({name:'somenameOther222'});
+      Session.set("enemy", "Eurasia");
+    }
+  });
+  Template.currentCard.helpers({
+    Card: function() {
+      return Session.get("enemy");
+      //return Cards.findOne({name:'somenameOther222'});
+    }
+  });
+
+
+
+
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
+
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+}
+
+if (Meteor.isServer) {
+  if (Cards.find().count() === 0){
+    //Cards.insert({name:'somename', attr1:5});
+
+  }
+
+
+  Meteor.methods({
+    newGame: function () {
+      // ...
+    },
+    nextRound: function () {
+      // ...
+    },
+    nextMove: function (player, selection, card){
+      
+    }
+  });
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
